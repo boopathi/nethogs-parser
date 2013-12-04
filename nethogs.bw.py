0d5f6c94b705d10a110b84bfb669924f4f086c7e
Boopathi Rajaa <me@boopathi.in>
2013-12-04 20:13:13 +0530

    Enhanced few things

--- nethogs.bw.py
+++ nethogs.bw.py
@@ -2,48 +2,78 @@
 
 import time,sys
 
-Data = {}
-
-timeout = time.time() + int(sys.argv[1])
-
 def watch():
-	while True:
-		try:
-			new = raw_input()
-		except (EOFError,KeyboardInterrupt,SystemExit):
-			yield(False)
-		if new:
-			yield(new)
-		else:
-			try:
-				time.sleep(0.5)
-			except (KeyboardInterrupt, SystemExit):
-				yield(False)
+  while True:
+    try: new = raw_input()
+    except (EOFError, KeyboardInterrupt, SystemExit): yield(False)
+    if new: yield(new)
+    else:
+      try: time.sleep(0.5)
+      except (KeyboardInterrupt, SystemExit): yield(False)
 
-count = 0
-for l in watch():
-	if l == False:
-		break
-	line = l.split()
-	if len(line) != 3:
-		continue
-	d = line[0].split('/')
-	if len(d) < 3:
-		continue
-	sent = float(line[1])
-	recv = float(line[2])
-	prog = '/'.join(d[0:-2])
-	pid  = d[-2]
-	uid  = d[-1]
-	count += 1
-	print '\r>> %d seconds remaining; %d lines processed' % (timeout - time.time(), count),
-	sys.stdout.flush()
-	#print pid, uid, prog, recv, sent
-	if not Data.has_key(prog):
-		Data[prog] = (0.0,0.0)
-	Data[prog] = tuple(sum(x) for x in zip(Data[prog],(sent,recv)))
-	if time.time() > timeout:
-		break
+class NH:
+  def __init__(self, timeout=60, showprogress=False):
+    self.data = {}
+    self.timeout = time.time() + int(timeout)
+    self.count = 0
+    self.showprogress = showprogress
+    self.runcomplete = False
 
-print
-print Data
+  def parseline(self,line):
+    ld = {}
+    l = line.split()
+    if len(l) != 3: return False
+    d = l[0].split('/')
+    if len(d) < 3: return False
+    self.count += 1
+    ld['sent'] = float(l[1])
+    ld['recv'] = float(l[2])
+    ld['prog'] = '/'.join(d[0:-2])
+    ld['pid']  = d[-2]
+    ld['uid']  = d[-1]
+    return ld
+
+  def run(self):
+    for l in watch():
+      if l == False: break
+      line = parseline(l)
+      if line == False: break
+      if not self.data.has_key(line['prog']):
+        self.data[line['prog']] = (0.0,0.0)
+      zipped = zip(self.data[line['prog']], (line['sent'],line['recv']))
+      self.data[line['prog']] = tuple(sum(x) for x in zipped)
+      if self.showprogress: self.progress()
+      if time.time() > self.timeout: break
+    self.runcomplete = True
+
+  def progress(self):
+    print '\r>> %d seconds remaining; %d lines processed' % (timeout - time.time(), count),
+    sys.stdout.flush()
+
+  def get(self):
+    return (self.runcomplete, self.data)
+
+  def fmtitem(self, item):
+    item = str(item)
+    if len(item) < 7:
+      item = item + "\t"
+    item = item + "\t"
+    return item
+
+  def pp(self):
+    if not self.runcomplete:
+      print "call method run to populate data"
+      return
+    else:
+      for prog in self.data:
+        sent = self.data['prog'][0]
+        recv = self.data['prog'][1]
+        print self.fmtitem(prog),
+        print self.fmtitem(sent),
+        print self.fmtitem(recv)
+
+if __name__ == "__main__":
+  d = NH(timeout=60, showprogress=True)
+  d.run()
+  data = d.get()
+
