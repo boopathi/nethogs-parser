2b784e35be5a72085e2a93ef96f6af51bd45c119
Boopathi Rajaa <me@boopathi.in>
2013-12-13 10:59:41 +0530

    Some modifications and bug fixes

--- hogs.go
+++ hogs.go
@@ -7,27 +7,38 @@ import (
   "os"
   "log"
   "io/ioutil"
+  "os/user"
   "strings"
   "strconv"
   "net/http"
   "net/url"
 )
 
+type set map[string]struct{}
+
+func (s set) add(x string) set {
+  if s == nil {
+    s = make(set)
+  }
+  s[x] = struct{}{}
+  return s
+}
+
+func (s set) get() []string {
+  x:=[]string{}
+  for i := range s{
+    x = append(x,i)
+  }
+  return x
+}
+
 type pt struct {
   sent float64
   recv float64
+  users set
 }
 
 var data map[string]pt
-
-func adddata(s string, p pt) {
-  if _,ok := data[s]; ok {
-    p2 := pt{data[s].sent + p.sent, data[s].recv+p.recv}
-    data[s] = p2
-  } else {
-    data[s] = pt{0.0,0.0}
-  }
-}
 
 func check(err error) {
   if err != nil {
@@ -44,7 +55,7 @@ var (
 func main() {
   data = map[string]pt {}
   flag.StringVar(&datatable, "datatable", "", "Datatable server details")
-  flag.BoolVar(&pprint, "prettyprint", false, "Pretty Print")
+  flag.BoolVar(&pprint, "pp", false, "Pretty Print")
   flag.Parse()
   filename = flag.Args()[0]
   file, err := os.Open(filename)
@@ -78,12 +89,21 @@ func parseline(line string) {
   if strings.Index(pname, ":") != -1 && strings.Index(pname, "-") != -1 {
     pname = strings.Split(pname, "-")[0]
   }
-  adddata(pname, pt{ sent, recv })
+  ud, err := user.LookupId(processdata[len(processdata)-1])
+  if err != nil {
+    ud = &user.User { "-1", "-1", "dontknow", "dontknow", "/home/dontknow" }
+  }
+  data[pname] = pt{
+    data[pname].sent + sent,
+    data[pname].recv + recv,
+    data[pname].users.add(ud.Username),
+  }
 }
 
 func prettyprint() {
   for proc, _ := range data {
-    fmt.Printf("%40s\t%10.2f\t%10.2f\n", proc, data[proc].sent, data[proc].recv)
+    fmt.Printf("%40s\t%10.2f\t%10.2f\t%40s\n", proc, data[proc].sent, data[proc].recv,
+      strings.Join(data[proc].users.get(), ",") )
   }
 }
 
@@ -91,8 +111,10 @@ func getcsv() string{
   csv := ""
   hostname,_ := os.Hostname()
   for proc, _ := range data {
-    csv = csv + fmt.Sprintf("%s,%.2f,%.2f,%s,%s\n",
-      proc, data[proc].sent, data[proc].recv,filename,hostname)
+    csv = csv + fmt.Sprintf("%s,%.2f,%.2f,%s,%s,%s\n",
+      proc, data[proc].sent, data[proc].recv,
+      strings.Join(data[proc].users.get(), " "),
+      filename,hostname)
   }
   return csv
 }
